fc888af12551d18dc1b16c07d019f9e01d623323
domainsi <48432637+domainsi@users.noreply.github.com>
2020-01-16 18:45:38 +0700

    Create Game.sh

---
+++ Game.sh
@@ -0,0 +1,72 @@
+clear
+blue='\033[1;34m'
+green='\033[1;32m'
+red='\033[1;35m'
+white='\033[1;37m'
+yellow='\033[1;36m'
+sleep1
+echo
+toilet -f big -F gay by Metro
+echo $blue"Selamat Datang Bang Boy"
+echo
+sleep 2
+echo $white"Pilihan Toolsnya :"
+echo
+echo $yellow"1.) Stabilkan Jaringan Free Fire"
+echo
+echo $yellow"2.) Stabilkan Jaringan Mobile Legend"
+echo
+echo $yellow"3.) Stabilkan Jaringan scroll on myoji"
+echo
+echo $yellow"4.) Stabilkan Jaringan Growtopia"
+echo
+echo $yellow"5.) Keluar"
+echo
+echo $blue
+read -p "pilihan: " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Metro
+echo $red"Stabilkan Jaringan Free Fire by Geming"
+sleep 2
+ping -s1000 201.182.152.79
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toliet -f big -F gay Metro
+echo $green"Stabilkan Jaringan Mobile Legend by Geming"
+sleep 2
+ping -s1000 45.230.176.4
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+toliet -f big -F gay Metro
+echo $white"Stabilkan Jaringan Scroll on myoji by Geming"
+sleep 2
+ping -s1000 185.54.237.114
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet -f big -F gay Metro
+echo $blue"Stabilkan Jaringan Growtopia by Geming"
+sleep 2
+ping -s1000 85.90.215.101
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+echo $yellow
+figlet "By Metro"
+echo $blue"To Be Continued"
+sleep3
+exit
+fi
